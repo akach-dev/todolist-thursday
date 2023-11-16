1c8f5f031073935c7b854fb648c800f56d8f7cc5
Alexander Kachan <akach.dev@mail.ru>
2023-11-16 20:58:15 +0300

    lesson 16 FORMIK validate

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -12,30 +12,37 @@ import Container from '@mui/material/Container';
 import LinearProgress from '@mui/material/LinearProgress';
 import {Menu} from '@mui/icons-material';
 import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
+import {Navigate, Route, Routes} from "react-router-dom";
+import {Login} from "../features/Login/Login";
 
 
 function App() {
-    const status = useAppSelector<RequestStatusType>((state) => state.app.status)
-    return (
-        <div className="App">
-            <ErrorSnackbar/>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
-                        <Menu/>
-                    </IconButton>
-                    <Typography variant="h6">
-                        News
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-                {status === 'loading' && <LinearProgress/>}
-            </AppBar>
-            <Container fixed>
-                <TodolistsList/>
-            </Container>
-        </div>
-    )
+  const status = useAppSelector<RequestStatusType>((state) => state.app.status)
+  return (
+     <div className="App">
+       <ErrorSnackbar/>
+       <AppBar position="static">
+         <Toolbar>
+           <IconButton edge="start" color="inherit" aria-label="menu">
+             <Menu/>
+           </IconButton>
+           <Typography variant="h6">
+             News
+           </Typography>
+           <Button color="inherit">Login</Button>
+         </Toolbar>
+         {status === 'loading' && <LinearProgress/>}
+       </AppBar>
+       <Container fixed>
+         <Routes>
+           <Route path={'/'} element={<TodolistsList/>}/>
+           <Route path={'/login'} element={<Login/>}/>
+           <Route path='/404' element={<h1>404: PAGE NOT FOUND</h1>}/>
+           <Route path={'*'} element={<Navigate replace to={'/404'}/>}/>
+         </Routes>
+       </Container>
+     </div>
+  )
 }
 
 export default App
